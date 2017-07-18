abf43a849f9332b1afcc06e91a4f656cedb1d545
BrieannaMann <brieanna.mann1@gmail.com>
2017-07-18 12:04:50 -0400

    resize update
    refreshing full page commented out

--- stylesheets/jquery.bxslider/slider.js
+++ stylesheets/jquery.bxslider/slider.js
@@ -1,68 +1,34 @@
 $(document).ready(function(){
 
-/*
-  $('.bxslider').bxSlider({
-    slideWidth: 350,
-    minSlides: 1,
-    maxSlides: 4,
-    moveSlides: 1,
-    slideMargin: 10
-});
-})  */
-  
-	// var orgProductWidth = $(".bxslider").children().eq(0).outerWidth;
-	// console.log(orgProductWidth);
-/*
-	var maxSlides,
-	width = $(window).width();
-
-	if (width < 600) {
-		minSlides =1;
-	    maxSlides = 1;
-	    console.log("error")
-	    console.log(maxSlides)
-	} else if (width <1000) {
-	    maxSlides = 2;
-	} else {
-		maxSlides = 4;
-	}
-
-	console.log(maxSlides)
-	
-	$('.bxslider').bxSlider({
-	    minSlides: 2,
-	    // slideWidth : orgProductWidth,
-    	maxSlides: 3,
-	});
-});
-*/
 var width =$(window).width();
 
 	if (width < 600) {
 	   $('.bxslider').bxSlider({
 	   maxSlides:1,
 	   minSlides:1,
-	   slideWidth: 314,
+	   slideWidth: 300,
 	 });
-	} else if (width < 1000){
+	} else if (width < 1100){
 	 $('.bxslider').bxSlider({
 	   maxSlides:2,
 	   minSlides:2,
-	   slideWidth: 314,
+	   slideWidth: 300,
 	 });
 	} else {
 	 $('.bxslider').bxSlider({
 	   maxSlides:4,
 	   minSlides:4,
-	   slideWidth: 314,
+	   slideWidth: 300,
 	 });
 	}; 
-	$(window).on('resize',function(){
-    if (width > 0) {  
+	/* $(window).on('resize',function(){
+
+   if (width > 0) {  
       location.reload(); 
-    }
-});
-})
+    } 
+});  */
+
+ })
 
 
 
